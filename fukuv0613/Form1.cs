dbb060c199dc12e7b684a90039e981d6fdc678da
nagayamamana <nagamiyama1031@gmail.com>
2025-06-13 15:34:08 +0900

    ha

--- fukuv0613/Form1.cs
+++ fukuv0613/Form1.cs
@@ -18,8 +18,18 @@ namespace fukuv0613
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Left += vx;
-            label1.Top += vy;   
+            label1.Top += vy;
+            if (label1.Left < 0)
+            {
+                label1.Left = 0;
+                vx = -vx;
+            }
+           
+            
+            
         }
+
     }
+    
 
 }
