44abad30f4f635d47a5134ec76609dd8cd15aa8a
nagayamamana <nagamiyama1031@gmail.com>
2025-06-13 13:58:26 +0900

    a

--- fukuv0613/Form1.cs
+++ fukuv0613/Form1.cs
@@ -2,9 +2,24 @@ namespace fukuv0613
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            vx = -vx;
+            vy = -vy;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;   
+        }
     }
+
 }
